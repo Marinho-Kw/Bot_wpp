dda260c7833fb6092c6939fc8432b1c0b279d131
Marinho <guilherme162830@gmail.com>
2021-08-11 17:56:03 -0300

    md2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Bot_wpp
- 
+# Py_bot
+
+Esse programa foi teste para ver se eu conseguia automatizar mensagens do WhatsApp afim de aprender mais sobre automação de tarefas
--- bot/auto.py
+++ bot/auto.py
@@ -3,10 +3,12 @@ import keyboard
 import time
 from datetime import datetime
 
-contatos = ['+5521976005140']
+contatos = ['(numero)']
 while len(contatos) >= 1:
-    pywhatkit.sendwhatmsg(contatos[0], 'essa mensagem foi programada por mim mãe :)',datetime.now().hour,datetime.now().minute + 2)
+    pywhatkit.sendwhatmsg(contatos[0], '(mensagem)',datetime.now().hour,datetime.now().minute + 2)
     del contatos[0]
     time.sleep(60)
     keyboard.press_and_release('ctrl + w')
+    
+    
     
